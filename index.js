51b9848a3ed5b90e9052892d937dff83fb53431c
Laurens <laurens.aarnoudse@gmail.com>
2019-05-17 22:38:15 +0200

    Set up a basic system that waits for an effect to finish and resolves a promise when it does. It's pretty hacky right now as waiting for an array of timeouts is annoying. I'll prob fix this eventually when switching to an animation library 🤷🏼‍♂️

--- index.js
+++ index.js
@@ -7,8 +7,8 @@
 
 const colors = ['DAA520','4B2295', '263596', 'DAC320']
 const svg = document.querySelector('svg')
-const allPaths = document.querySelectorAll("path")
-const triangles = document.querySelectorAll("#triangles path")
+const allPaths = Array.from(document.querySelectorAll("path"))
+const triangles = Array.from(document.querySelectorAll("#triangles path"))
 const triang_lefts = Array.from(document.querySelectorAll("#triang_x5F_left path"))
 const triang_rights = Array.from(document.querySelectorAll("#triang_x5F_right path"))
 const third_eye = document.querySelector('#third_x5F_eye')
@@ -21,20 +21,21 @@ third_eye.addEventListener("click", function(){flashAnim(triang_lefts)})
 disco_eye.addEventListener("click", function(){lightEmUp(allPaths)})
 //svg.addEventListener("click", function(){showOnebyOne(allPaths)})
 
-
-
 orderBy(triang_lefts, 'y', false)
 orderBy(triang_rights, 'y', true)
+orderBy(allPaths, 'x', false)
 // svg.addEventListener("click", function(){lightEmUp(triang_lefts)})
-svg.addEventListener("click", function(){paintIt(triang_lefts, 'goldenrod')})
-svg.addEventListener("click", function(){paintIt(triang_rights, 'goldenrod')})
-
+svg.addEventListener("click", function(){
+  paintIt(triang_lefts, 'goldenrod', 30)
+    .then(() => paintIt(triang_rights, 'goldenrod', 30))
+    .then(() => paintIt(allPaths, 'darkgreen', 10))
+})
+//svg.addEventListener("click", function(){paintIt(triang_rights, 'goldenrod', 100)})
 
 function orderBy(arr, prop, ascending){
   if (ascending) return arr.sort((a, b) => a.getBoundingClientRect()[prop] - b.getBoundingClientRect()[prop])
   else return arr.sort((b, a) => a.getBoundingClientRect()[prop] - b.getBoundingClientRect()[prop])
 }
-   
 
 function showOnebyOne(arr){
   //console.log("triggered") 
@@ -57,9 +58,17 @@ function flashAnim(arr){
   })
 }
 
-function paintIt(collection, color){
-  let elements = Array.isArray(collection) ? collection : [collection]
-  elements.forEach((t, i) => {
-    setTimeout(() => t.style.fill = color, 20 * i)
+function paintIt(collection, color, duration){
+  //Fix: Can't wait for each of the timeouts. Probable solution here: https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
+  return new Promise( (resolve,reject) => {
+    let elements = Array.isArray(collection) ? collection : [collection]
+    elements.forEach((t, i) => {
+      setTimeout(() => t.style.fill = color, duration * i)
+    })
+    setTimeout(() => {  //DIRTY DIRTY HACKS
+      console.log("done!")
+      resolve(elements.length)
+    }, duration * elements.length)
+    console.log("All paints done?")
   })
 }
