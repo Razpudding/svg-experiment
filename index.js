ad585380264621ff26d41e1199fdba41ee6c4848
Laurens <laurens.aarnoudse@gmail.com>
2019-05-17 21:48:15 +0200

    Rewrote event handlers to accept parameters. Added an ordering function that can dynamically order svg elements by a dynamic boundingbox property. Next up is promisifying some of the animation functions

--- index.js
+++ index.js
@@ -9,20 +9,36 @@ const colors = ['DAA520','4B2295', '263596', 'DAC320']
 const svg = document.querySelector('svg')
 const allPaths = document.querySelectorAll("path")
 const triangles = document.querySelectorAll("#triangles path")
-const triang_lefts = document.querySelectorAll("#triang_x5F_left path")
+const triang_lefts = Array.from(document.querySelectorAll("#triang_x5F_left path"))
+const triang_rights = Array.from(document.querySelectorAll("#triang_x5F_right path"))
 const third_eye = document.querySelector('#third_x5F_eye')
 const disco_eye = document.querySelector('#disco_x5F_eye')
 
 let allHidden = true
 //triang_lefts.forEach(t => t.addEventListener("mouseover", makeGolden))
 //triang_lefts.forEach(t => t.addEventListener("mouseout", makeBlack))
-third_eye.addEventListener("click", flashAnim)
-disco_eye.addEventListener("click", lightEmUp)
-// svg.addEventListener("click", showOnebyOne)
+third_eye.addEventListener("click", function(){flashAnim(triang_lefts)})
+disco_eye.addEventListener("click", function(){lightEmUp(allPaths)})
+//svg.addEventListener("click", function(){showOnebyOne(allPaths)})
 
-function showOnebyOne(){
+
+
+orderBy(triang_lefts, 'y', false)
+orderBy(triang_rights, 'y', true)
+// svg.addEventListener("click", function(){lightEmUp(triang_lefts)})
+svg.addEventListener("click", function(){paintIt(triang_lefts, 'goldenrod')})
+svg.addEventListener("click", function(){paintIt(triang_rights, 'goldenrod')})
+
+
+function orderBy(arr, prop, ascending){
+  if (ascending) return arr.sort((a, b) => a.getBoundingClientRect()[prop] - b.getBoundingClientRect()[prop])
+  else return arr.sort((b, a) => a.getBoundingClientRect()[prop] - b.getBoundingClientRect()[prop])
+}
+   
+
+function showOnebyOne(arr){
   //console.log("triggered") 
-  allPaths.forEach((t,i) => {
+  arr.forEach((t,i) => {
     setTimeout(() => t.style.display = "block", 50 * i)
   })
   allHidden = false
@@ -30,22 +46,20 @@ function showOnebyOne(){
 
 function lightEmUp(arr){
   const seed = Math.floor(Math.random() * 3)
-  allPaths.forEach((t, i) => {
-    setTimeout(() => t.style.fill = colors[(i+seed)%3], 10 * i)
+  arr.forEach((t, i) => {
+    setTimeout(() => t.style.fill = colors[(i+seed)%3], 20 * i)
   })
 }
 
-function flashAnim(){  
-  triang_lefts.forEach((t, i) => {
+function flashAnim(arr){  
+  arr.forEach((t, i) => {
     setTimeout(() => t.style.fill = "goldenrod", 50 * i)
   })
 }
 
-function makeGolden(){
-  //console.log("triggered", this)
-  this.style.fill = "goldenrod"
-}
-
-function makeBlack(){
-  this.style.fill = "black"
+function paintIt(collection, color){
+  let elements = Array.isArray(collection) ? collection : [collection]
+  elements.forEach((t, i) => {
+    setTimeout(() => t.style.fill = color, 20 * i)
+  })
 }
